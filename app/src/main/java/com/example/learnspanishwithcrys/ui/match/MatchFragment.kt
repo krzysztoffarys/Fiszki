fbc25ff77c914bb0ca221e82e9d52aaf3462eeb4
crys <krzysztoffarys00@gmail.com>
2021-10-30 09:49:33 +0200

    init

---
+++ app/src/main/java/com/example/learnspanishwithcrys/ui/match/MatchFragment.kt
@@ -0,0 +1,7 @@
+package com.example.learnspanishwithcrys.ui.match
+
+import androidx.fragment.app.Fragment
+import com.example.learnspanishwithcrys.R
+
+class MatchFragment : Fragment(R.layout.match_fragment) {
+}
